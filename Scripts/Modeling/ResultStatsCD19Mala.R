a1885d3b37ad87d1340d5591b18d782c101cb1e3
Lee Panter <lee.panter@yahoo.com>
2020-02-22 10:30:53 -0700

    Push V4 revision and V3 comments

---
+++ Scripts/Modeling/ResultStatsCD19Mala.R
@@ -0,0 +1,94 @@
+# ResultStatsCD19Mala.R
+# Nested Model Comparisons and Percent Change Matrices
+
+
+# Load Data
+load("/Users/lee/Desktop/CD19MalaWS.RData")
+
+# Packages
+library(nlme)
+library(stargazer)
+
+# We first Calculate the nested model comparisons
+
+loglmod0.nlme=gls(model = logmala~logcd19,
+                  data = dat)
+
+logLMMwREint.nlme=lme(fixed=logmala~1 + logcd19,
+                        random=~1|subject.no,
+                        data = dat)
+
+logLMMwREslope.nlme=lme(fixed=logmala~1 + logcd19,
+                        random=~(1+logcd19|subject.no),
+                        data = dat)
+
+## Model 0 < Model 1
+anova(loglmod0, logLMwFEint)
+
+## Model 0 < Model 3
+x=anova.lme(loglmod0.nlme,logLMMwREint.nlme)
+
+## Model 0 < Model 3
+anova.lme(loglmod0.nlme,logLMMwREslope.nlme)
+
+
+
+# Define Percent Change Function
+PerctChange=function(bef,aft){
+  diff=aft-bef
+  out=round(diff/((aft+bef)/2), digits = 4)
+  return(out)
+}
+
+
+# Function to calculate Percent Change Matrices
+PerctChangeMatrix=function(vec){
+  len=length(vec)
+  out.mat=matrix(NA, nrow = len, ncol = len)
+  for(i in 1:len){
+    for(j in 1:len){
+      out.mat[i,j]=PerctChange(vec[i], vec[j])
+    }
+  }
+  return(out.mat)
+}
+
+
+# Create Coefficient Vector:
+
+coef.loglmod=4.918*10**(-2)
+coef.logLMwFEint=4.833*10**(-2)
+coef.logLMMwREint=4.92*10**(-2)
+coef.logLMMwREslope=5.938*10**(-2)
+coef.logGEE=2.22*10**(-1)
+
+# Calculate Coeficient percentage of change
+coef.CD19Mala=c(coef.loglmod,coef.logLMwFEint,coef.logLMMwREint,
+                coef.logLMMwREslope,coef.logGEE)
+(perctChange.coefCD19Mala=PerctChangeMatrix(coef.CD19Mala))
+
+model.names=c("Mod 0", "Mod 1", "Mod 2", "Mod 3", "Mod 4")
+
+colnames(perctChange.coefCD19Mala)=model.names
+rownames(perctChange.coefCD19Mala)=model.names
+
+perctChange.coefCD19Mala.df=data.frame(perctChange.coefCD19Mala)
+
+
+# Create Standard Error Vector
+se.loglmod        =1.455*10 **(-2)
+se.logLMwFEint    =1.381*10 **(-2)
+se.logLMMwREint   =1.374*10  **(-2)
+se.logLMMwREslope =3.538*10 **(-2)
+se.logGEE         =6.3*10  **(-1)
+
+# Calculate SE Percent Change matrix
+se.CD19Mala=c(se.loglmod,
+              se.logLMwFEint,
+              se.logLMMwREint,
+              se.logLMMwREslope,
+              se.logGEE)
+
+(perctChange.seCD19Mala=PerctChangeMatrix(se.CD19Mala))
+
+
---
+++ Scripts/Modeling/ResultStatsCD34Fbln.R
@@ -0,0 +1,94 @@
+# ResultStatsCD34Fbln.R
+# Nested Model Comparisons and Percent Change Matrices
+
+
+# Load Data
+load("/Users/lee/Desktop/CD34FblnWS.RData")
+
+# Packages
+library(nlme)
+library(stargazer)
+
+# We first Calculate the nested model comparisons
+
+loglmod0.nlme=gls(model = logfbln~logcd34,
+                  data = dat)
+
+logLMMwREint.nlme=lme(fixed=logfbln~1 + logcd34,
+                      random=~1|subject.no,
+                      data = dat)
+
+logLMMwREslope.nlme=lme(fixed=logfbln~1 + logcd34,
+                        random=~(1+logfbln|subject.no),
+                        data = dat)
+
+## Model 0 < Model 1
+anova(loglmod0, logLMwFEint)
+
+## Model 0 < Model 3
+x=anova.lme(loglmod0.nlme,logLMMwREint.nlme)
+
+## Model 0 < Model 3
+anova.lme(loglmod0.nlme,logLMMwREslope.nlme)
+
+
+
+# Define Percent Change Function
+PerctChange=function(bef,aft){
+  diff=aft-bef
+  out=round(diff/((aft+bef)/2), digits = 4)
+  return(out)
+}
+
+
+# Function to calculate Percent Change Matrices
+PerctChangeMatrix=function(vec){
+  len=length(vec)
+  out.mat=matrix(NA, nrow = len, ncol = len)
+  for(i in 1:len){
+    for(j in 1:len){
+      out.mat[i,j]=PerctChange(vec[i], vec[j])
+    }
+  }
+  return(out.mat)
+}
+
+
+# Create Coefficient Vector:
+
+coef.loglmod=7.884*10**(-1)
+coef.logLMwFEint=1.306*10**(-1)
+coef.logLMMwREint=1.35*10**(-1)
+coef.logLMMwREslope=1.705*10**(-1)
+coef.logGEE=7.88*10**(-1)
+
+# Calculate Coeficient percentage of change
+coef.CD34Fbln=c(coef.loglmod,coef.logLMwFEint,coef.logLMMwREint,
+                coef.logLMMwREslope,coef.logGEE)
+(perctChange.coef.CD34Fbln=PerctChangeMatrix(coef.CD34Fbln))
+
+model.names=c("Mod 0", "Mod 1", "Mod 2", "Mod 3", "Mod 4")
+
+colnames(perctChange.coef.CD34Fbln)=model.names
+rownames(perctChange.coef.CD34Fbln)=model.names
+
+perctChange.coef.CD34Fbln.df=data.frame(perctChange.coef.CD34Fbln)
+
+
+# Create Standard Error Vector
+se.loglmod        =4.92*10**(-2)
+se.logLMwFEint    =3.42*10**(-2)
+se.logLMMwREint   =3.42*10**(-2)
+se.logLMMwREslope =7.29*10**(-2)
+se.logGEE         =4.69*10**(-1)
+
+# Calculate SE Percent Change matrix
+se.CD34Fbln=c(se.loglmod,
+              se.logLMwFEint,
+              se.logLMMwREint,
+              se.logLMMwREslope,
+              se.logGEE)
+
+(perctChange.seCD34Fbln=PerctChangeMatrix(se.CD34Fbln))
+
+
